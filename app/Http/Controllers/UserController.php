6abea18ca3dfe474798f2b787c9cd1fa3d1e64bf
hoangvanngoc <hoangngocbkhn2311@gmail.com>
2023-08-22 23:36:40 +0700

    validate create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -24,5 +25,52 @@ class UserController extends Controller
             ->get();
 
         return response()->json($users);
+    }
+
+    public function create() {
+        $user_status = \DB::table("user_status")->select(
+            "id as value",
+            "name as label"
+        )->get();
+        $departments = \DB::table("departments")->select(
+            "id as value",
+            "name as label"
+        )->get();
+
+
+        return response()->json([
+            "user_status" => $user_status,
+            "departments" =>  $departments
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'status_id' => 'required',
+            'username' => 'required|unique:users,username',
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'departments' => 'required',
+            'password' => 'required|confirmed'
+        ],
+        [
+          'status_id.require' => 'This field is required',
+          'username.required' => 'This field is required',
+          'username.unique' => "This username alrealy exist!",
+
+          'name.required' => 'This field is required',
+          'name.max' => 'Name is maximum 255 characters',
+
+          'email.required' => 'This field is required',
+          'email.email' => 'Invalid email',
+
+          'departments.required' => 'This field is required',
+
+          'password.required' => 'This field is required',
+          'password.confirmed' => 'Password is not match'
+        ]);
+
+        return $validated;
     }
 }
